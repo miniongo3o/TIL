a8faa8d9cd8750fced42bf2ca4c58532262a01ed
miniongo3o <dlalsdyd094@naver.com>
2020-09-07 01:59:08 +0900

    TIL 시작

--- programmers/0907.py
+++ programmers/0907.py
@@ -6,6 +6,7 @@ def solution(tick):
     answer = []
     temp = []
     visit = [0] * len(tick)
+    # 가능한 경로가 많을 때, 알파벳 우선순위가 있으므로 정렬
     tick.sort()
     dfs("ICN", temp, 0, tick, visit, answer)
     print(final)
@@ -24,6 +25,8 @@ def dfs(start, temp, cnt, tickets, visit, answer):
             success = dfs(tickets[i][1], temp, cnt + 1, tickets, visit, answer)
             if success:
                 return True
+            # 핵심포인트. dfs가 성공했다면 True를 반환 -> 계속 이어감.
+            # False를 반환했다면, 연결이 되지 않는다. -> i는 방문하지 않은 것.
             visit[i] = 0
     temp.pop()
     return False

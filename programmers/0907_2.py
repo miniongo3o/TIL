ccbe1f5d9530f5971f84f208704619f56023a6a3
miniongo3o <dlalsdyd094@naver.com>
2020-09-07 22:23:23 +0900

    TIL 2일차. 프로그래머스 heap 더 맵게! 문제

---
+++ programmers/0907_2.py
@@ -0,0 +1,21 @@
+import heapq
+def solution(sco, K):
+    answer = 0
+    heapq.heapify(sco)
+    while True:
+        temp=heapq.heappop(sco)
+        if temp>=K:
+            heapq.heappush(sco,temp)
+            break
+        else:
+            if len(sco)==0:
+                return -1
+            temp2=heapq.heappop(sco)
+            heapq.heappush(sco,temp+(temp2*2))
+            answer+=1
+
+    return answer
+
+
+
+print(solution([2,1,3,9,10,12],7))

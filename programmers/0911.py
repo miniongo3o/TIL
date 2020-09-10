df62e7feb3322486caa59d104d9fa7db7d1d69fb
miniongo3o <dlalsdyd094@naver.com>
2020-09-10 22:56:11 +0900

    TIL 5일차 프로그래머스 DP 정수삼각형

---
+++ programmers/0911.py
@@ -0,0 +1,25 @@
+def solution(tri):
+
+
+
+    answer=0
+    h=len(tri)
+
+    result=0
+    for i in range(1,h):
+        for j in range(len(tri[i])):
+
+            if j==0 :
+                tri[i][j]+=tri[i-1][j]
+            elif j==i:
+                tri[i][j]+=tri[i-1][j-1]
+            else:
+                a=tri[i][j]+tri[i-1][j]
+                b=tri[i][j]+tri[i-1][j-1]
+                tri[i][j]=max(a,b)
+
+
+
+
+
+    return max(tri[h-1])

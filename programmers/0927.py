d967193a1d28712b7d2b7867e6ca53573cf75ce7
miniongo3o <dlalsdyd094@naver.com>
2020-09-28 22:37:04 +0900

    TIL 0928 commit

---
+++ programmers/0927.py
@@ -0,0 +1,26 @@
+y,x=map(int,input().split())
+
+# 1 : 상하/좌우
+# 2 : 
+
+
+
+
+arr=[]
+camera=[]
+for i in range(y):
+    temp=list(map(int,input().split()))
+    for j in range(len(temp)):
+        if 0<temp[j]<6:
+            camera.append([i, j])
+
+    arr.append(temp)
+
+for i in arr:
+    print(i)
+
+
+print(camera)
+
+
+

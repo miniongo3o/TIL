99bc00ef72b47182ed9af196a56f3b086e745c6b
miniongo3o <dlalsdyd094@naver.com>
2020-10-02 21:24:31 +0900

    TIL 1002 프로그래머스 최솟값만들기

---
+++ programmers/1002.py
@@ -0,0 +1,11 @@
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+
+    sums = 0
+    for i, j in zip(A, B):
+        sums += (i * j)
+    answer = sums
+
+    return answer

71fa318d2e2ae7cdafdf3a949770b5433de5e988
miniongo3o <dlalsdyd094@naver.com>
2020-10-27 15:59:11 +0900

    1027 프로그래머스 스킬트리

---
+++ programmers/1027.py
@@ -0,0 +1,19 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for i in skill_trees:
+        temp = ""
+        for j in i:
+            if j in skill:
+                temp += j
+        print(temp)
+        if temp in skill:
+            cnt=0
+            for i in range(len(temp)):
+                if temp[i]==skill[i]:
+                    cnt+=1
+
+            if cnt==len(temp):
+                answer+=1
+
+    return answer
+print(solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]))

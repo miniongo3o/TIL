7c9a7eabfbfe1890ca0c9e6ff34e193331f994f6
miniongo3o <dlalsdyd094@naver.com>
2021-02-20 19:16:43 +0900

    0220

--- algorithm/0220/SWEA_10059_유효기간.java
+++ algorithm/0220/SWEA_10059_유효기간.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SWEA_10059_유효기간 {
+	static String[] arr= {"NA","MMYY","YYMM","AMBIGUOUS"};
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
@@ -22,21 +23,14 @@ public class SWEA_10059_유효기간 {
 			if(s2.charAt(0)=='0') {
 				s2=Character.toString(s2.charAt(1));
 			}
-			System.out.println(s1+" "+s2);
 			int a1=Integer.parseInt(s1);
 			int a2=Integer.parseInt(s2);
+			int res=0;
+
+			if(a1>0&&a1<13)res+=1;
+			if(a2>0&&a2<13)res+=2;
+			sb.append("#"+t+" "+arr[res]+"\n");
 			
-			if(a1>12&&a2>12||a1==0&&a2==0) {
-				sb.append("#"+t+" "+"NA\n");
-			}else if(a1<13&&a2<13&&a1>0&&a2>0) {
-				sb.append("#"+t+" "+"AMBIGUOUS\n");
-				//앞의 것만 12넘음 = YYMM
-			}else if(a1<13) {
-				sb.append("#"+t+" "+"MMYY\n");
-				//뒤의것만 12넘음 = MMYY
-			}else if(a2>13) {
-				sb.append("#"+t+" "+"YYMM\n");
-			}
 			
 		}
 		System.out.println(sb);

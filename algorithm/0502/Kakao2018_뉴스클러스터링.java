defd131f5da0a6d73df517cda872fac28b3a1ab1
miniongo3o <dlalsdyd094@naver.com>
2021-05-02 20:16:48 +0900

    0502 solve3

--- algorithm/0502/Kakao2018_뉴스클러스터링.java
+++ algorithm/0502/Kakao2018_뉴스클러스터링.java
@@ -6,9 +6,11 @@ class Kakao2018_뉴스클러스터링 {
     public int solution(String str1, String str2) {
        int answer = 0;
 
+       // 교집합, 합집합을 구하기위한 2개의 해쉬맵
 		HashMap<String, Integer> hash1 = new HashMap();
 		HashMap<String, Integer> hash2 = new HashMap();
 
+		// 모두 소문자로 통일
 		str1 = str1.toLowerCase();
 		str2 = str2.toLowerCase();
 
@@ -16,23 +18,22 @@ class Kakao2018_뉴스클러스터링 {
 		makeHash(hash2, str2);
 
 	
-
-		Set<String> keyset = hash1.keySet();
+		// 교집합의 수 cnt1
 		int cnt1 = 0;
-
-	    
-
+		
+		// hash1이 가지고 있는 key를 기준으로 확인 
+		//= hash1에 없고 hash2에 있는 경우는 교집합아니니까 한번만 돌면 된다.
+		// 교집합은, 두개의 문자열의 공통 부분문자열의 최소값이다. 
 		if (hash2.size() != 0) {
 			for (String k : hash2.keySet()) {
-
 				if (hash1.get(k) != null) {
 					cnt1 += Math.min(hash1.get(k), hash2.get(k));
 				}
-
 			}
 		}
 		
-		
+		// 합집합의 수 cnt2
+		// 합집합의 수는 두개의 총 수에서 교집합수를 뺴주면 된다.
 		int cnt2 = 0;
 		for (String k : hash1.keySet()) {
 			cnt2 += hash1.get(k);
@@ -42,16 +43,18 @@ class Kakao2018_뉴스클러스터링 {
 		}
 		cnt2 -= cnt1;
 
-        
+		
+
         if(cnt1==0){
             if(cnt2==0){
+            	// 교집합과 합집합 둘다 없는 경우는 65536을 return
+            	// 둘다 공집합인 J(0,0)은 1으로 규정했음.
+            	//
                 return 65536;
             }
+            // 교집합이 없고 합집합만 있는경우는 0을 return
+            // 유사도가 없음
             return 0;
-        }
-        if(cnt2==0){
-            
-            return 65536;
         }
         
 		

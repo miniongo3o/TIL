689cf60bb73edd3731008fedab592d25ef2097ed
miniongo3o <dlalsdyd094@naver.com>
2021-06-08 19:58:15 +0900

    0608

--- algorithm/0608/BOJ_1339_단어수학.java
+++ algorithm/0608/BOJ_1339_단어수학.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BOJ_1339_단어수학 {
+	// pos 객체로 알파벳:현재 숫자를 표현, 정렬을 위해 override
 	static class pos implements Comparable<pos> {
 		int num;
 		char word;
@@ -28,10 +29,6 @@ public class BOJ_1339_단어수학 {
 	}
 
 	public static void main(String[] args) throws IOException {
-//		char a='A';
-//		System.out.println((int)(a-64));
-//		int b=1;
-//		System.out.println((char)(b+64));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] list = new int[27];
@@ -44,17 +41,24 @@ public class BOJ_1339_단어수학 {
 			int len = arr.length;
 			for (int j = 0; j < len; j++) {
 				int ten = 1;
+				// 인덱스마다 자릿수를 곱해준다.
+				// 0번 인덱스 = 가장 큰 수이므로 10^n-1 
 				for (int k = 1; k < len - j; k++) {
 					ten *= 10;
 				}
+				// 만약 2 AAA C 를 입력 받았을 때 
+				// p[1]=('A',111), p[3]=('C',1)
 				list[(int) (arr[j] - 64)] += ten;
 				p[(int) (arr[j] - 64)].num += ten;
 				p[(int) (arr[j] - 64)].word = arr[j];
 			}
 		}
+		// 알파벳 순이 아닌, 입력받은 수를 기준으로 내림차순으로 정렬해준다.
 		Arrays.sort(p);
 		int idx=9;
 		int sum=0;
+		// 알파벳은 10개 미만이므로 9부터 1까지 존재한다.
+		// 가장 큰 값을 가진 알파벳을 9부터 설정해주고 1씩 감소시킨다.
 		for (int i = 0; i <= 26; i++) {
 			if (p[i].num != 0) {
 				sum+=p[i].num*idx;
